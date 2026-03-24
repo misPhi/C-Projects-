085c1995da3f77579013a6daf2ef47632a0fbb15
misPhi <1254sdxz@protonmail.com>
2026-03-25 08:40:26 +0900

    sudoku.c の更新

--- 7 Sudoku Solver/sudoku.c
+++ 7 Sudoku Solver/sudoku.c
@@ -17,15 +17,13 @@ int valid_move(int puzzle[9][9], int row, int col, int val);
 int solve_puzzle(int puzzle[9][9]);
 
 int findEmpty(int puzzle[9][9]){
-int empty = -1;
 
 for(int i=0;i<9;i++){
 for(int j=0;j<9;j++){
 
 if (puzzle[i][j]==0){
-empty=i*9+j;
+return i*9+j;
 }
-return empty;
 
 }
 }
